44e82ab76a60ee90cd54657d1328dfd99717e20d
ibrahim ahmad <ibrahimahmad.info@gmail.com>
2024-04-29 17:21:27 +0800

    Update createCard function to clear input field once card is created, and to update the card deck array with the new card.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 const taskInputButton = document.getElementById('showInput');
 const addTaskButton = document.getElementById('createTask');
+let cardDeck = [];
 
 
 // add event listener
 taskInputButton.addEventListener('click', showInputDialog);
 addTaskButton.addEventListener('click', createCard);
+addTaskButton.addEventListener('click', closeInputDialog);
 
 // functions
 function showInputDialog() {
     const inputDialog = document.querySelector('dialog');
     inputDialog.showModal();
+}
+
+function closeInputDialog() {
+    const inputDialog = document.querySelector('dialog');
+    inputDialog.close();
 }
 
 function getInput() {
@@ -21,7 +28,17 @@ function getInput() {
     return {task, project, personInCharge, dueDate};
 }
 
+function clearInput() {
+    document.querySelector('input#taskName').value = '';
+    document.querySelector('input#projectName').value = '';
+    document.querySelector('input#personInCharge').value = '';
+    document.querySelector('input#dueDate').value = '';
+}
+
 function createCard() {
     const card = getInput();
     console.log(card);
+    clearInput();
+
+    cardDeck.push(card);
 }
