450cc168849c38c7bfa54e70d78d04e8747748a8
ibrahim ahmad <ibrahimahmad.info@gmail.com>
2024-05-20 21:18:45 +0800

    Reorganize functions as modules under broad grouping: events, tasks, and UI.

---
+++ src/task.js
@@ -0,0 +1,210 @@
+// This module will handle task creation, deletion, ranking, and summary operations.
+
+import { differenceInCalendarDays } from 'date-fns';
+import { dashboardDisplay, clearDashboard, summaryValueDisplay } from './ui';
+import { uploadToStorage} from './storage.js';
+
+function getInput() {
+    const task = document.querySelector('#input-task').value;
+    const project = document.querySelector('#input-project').value;
+    const personInCharge = document.querySelector('#input-personInCharge').value;
+    const dueDate = document.querySelector('#input-dueDate').value;
+    const status = document.querySelector('#input-status').value;
+
+    return {task, project, personInCharge, dueDate, status};
+}
+
+function clearInput(card) {
+
+    for (let key in card) {
+        let cardID = `#input-${key}`;
+        document.querySelector(cardID).value = '';
+    }
+    
+}
+
+function daysRemaining(date) {
+    const nowDate = new Date();
+    return differenceInCalendarDays(date, nowDate);
+    
+}
+
+function createCard(cardDeck) {
+    const card = getInput();
+    const daysRemain = daysRemaining(card.dueDate);
+    clearInput(card);
+    document.getElementById('input-summary').value = 'all';
+    document.getElementById('summary-value').value = '';
+    cardDeck.push(card);
+
+    return cardDeck;
+}
+
+function deleteCard(e, cardDeck) {
+    const card = e.target.parentNode;
+    let deleteID = getCardID(card);
+    cardDeck = deleteCardFromDeck(cardDeck, deleteID);
+
+    clearDashboard();
+    dashboardDisplay(cardDeck);
+    uploadToStorage(cardDeck);
+
+    return cardDeck;
+}
+
+function deleteCardFromDeck(cardDeck, deleteID){
+    
+    const cardIndex = findDeckIndex(cardDeck, deleteID);
+    cardDeck.splice(cardIndex,1);
+
+    return cardDeck;
+
+}
+
+function findDeckIndex(cardDeck, id){
+
+    for (let cardIndex in cardDeck){
+        let card = cardDeck[cardIndex];
+        const cardID = getCardID(card);
+        if (cardID === id) {return cardIndex}
+    }
+
+}
+
+function upRank(e, cardDeck) {
+    const currentNode = e.target.parentNode;
+    const prevNode = currentNode.previousElementSibling;
+
+    if (prevNode.className === 'dashboard-title') {return;}
+
+    const prevCardID = getCardID(prevNode);
+    const prevCardDeckIndex = findDeckIndex(cardDeck, prevCardID);
+
+    const currentCardID = getCardID(currentNode);
+    const currentCardDeckIndex = findDeckIndex(cardDeck, currentCardID);
+
+    // get the current object from deck
+    const currentCard = cardDeck[currentCardDeckIndex];
+    
+    // delete the current object from deck
+    cardDeck = deleteCardFromDeck(cardDeck, currentCardID);
+
+    // add the current card object based on the location of the previous card index
+    cardDeck.splice(prevCardDeckIndex,0,currentCard);
+
+    // regenerate the dashboard based on the updated deck
+    clearDashboard()
+    dashboardDisplay(cardDeck);
+    uploadToStorage(cardDeck);
+
+    return cardDeck;
+}
+
+function downRank(e, cardDeck) {
+    const currentNode = e.target.parentNode;
+    const nextNode = currentNode.nextElementSibling;
+
+    if (nextNode === null) {return;}
+
+    const nextCardID = getCardID(nextNode);
+    const nextCardDeckIndex = findDeckIndex(cardDeck, nextCardID);
+
+    const currentCardID = getCardID(currentNode);
+    const currentCardDeckIndex = findDeckIndex(cardDeck, currentCardID);
+
+    // get the current object from deck
+    const currentCard = cardDeck[currentCardDeckIndex];
+    
+    // delete the current object from deck
+    cardDeck = deleteCardFromDeck(cardDeck, currentCardID);
+
+    // add the current card object based on the location of the previous card index
+    cardDeck.splice(nextCardDeckIndex,0,currentCard);
+
+    // regenerate the dashboard based on the updated deck
+    clearDashboard()
+    dashboardDisplay(cardDeck);
+    uploadToStorage(cardDeck);
+
+    return cardDeck;
+}
+
+function getSummaryValue(e, cardDeck) {
+    const summaryValue = e.target.value;
+    let valueList = [];
+
+    for (let cardIndex in cardDeck){
+
+        const currentValue = cardDeck[cardIndex][summaryValue];
+        let valueExists = valueList.includes(currentValue); 
+
+        if (valueExists === false){
+            valueList.push(currentValue);
+        }
+
+    }
+
+    // display the selection for user to choose
+    summaryValueDisplay(valueList);
+
+}
+
+
+function getSummarizedDashboard(summaryElement, summaryValue, cardDeck) {
+
+    let summarizedDeck = cardDeck;
+
+    // update summarizedDeck if applicable
+    if (summaryElement !== 'all') {
+
+        // clear the deck first before updating
+        summarizedDeck = [];
+
+        for (let cardIndex in cardDeck) {
+            let card = cardDeck[cardIndex];
+    
+            if (card[summaryElement] === summaryValue) {
+                summarizedDeck.push(card);
+            }
+        }
+    }
+    
+    // display dashboard
+    clearDashboard();
+    dashboardDisplay(summarizedDeck);
+    
+}
+
+function getCardID(card){
+    const cardItem = ['task', 'project', 'personInCharge', 'dueDate'];
+    let cardObject = card;
+    let cardID = '';
+
+    // parameter is either a node or an array.
+    // if its not array, convert node info to array first.
+    let checkNode = card.nodeType // return undefined if it is an array
+
+    if (checkNode !== undefined) {
+        cardObject = {};
+
+        for (let itemIndex in cardItem) {
+            const input = card.querySelector(`.card-${cardItem[itemIndex]}`).textContent;
+            cardObject[cardItem[itemIndex]] = input;
+        }
+
+    } 
+
+    for (let itemIndex in cardItem){
+        let input = cardObject[cardItem[itemIndex]];
+        cardID = cardID + '_' + input;
+    }
+
+    return cardID;
+}
+
+export {
+    getInput, clearInput, daysRemaining,
+    createCard, deleteCard, deleteCardFromDeck,
+    findDeckIndex, upRank, downRank, getSummaryValue,
+    getSummarizedDashboard
+}
---
+++ src/ui.js
@@ -0,0 +1,122 @@
+// This module will handle all the UI-related functions, such as showing dialogs, updating the dashboard display, 
+// and managing user inputs.
+
+import {deleteCard, upRank, downRank} from './task'
+
+function showInputDialog() {
+    const inputDialog = document.querySelector('dialog');
+    inputDialog.showModal();
+}
+
+function closeInputDialog() {
+    const inputDialog = document.querySelector('dialog');
+    inputDialog.close();
+}
+
+function cardButton(buttonName){
+    const button = document.createElement('button');
+    button.setAttribute('id', `${buttonName}Card`);
+    button.textContent = buttonName;
+
+    return button;
+}
+
+function cardDisplay(card, cardDeck) {
+    const cardContainer = document.createElement('div');
+    cardContainer.setAttribute('class', 'card-container');
+
+    // add tasks information
+    for (let key in card) {
+        if (key === 'status') {continue;} // TODO: no need to display status info? what if want to update/edit status?
+
+        const div = document.createElement('div');
+        div.setAttribute('class', `card-${key}`);
+        div.textContent = card[key];
+
+        cardContainer.appendChild(div);
+    }
+
+    // add delete button
+    const deleteButton = cardButton('delete');
+    deleteButton.textContent = 'Delete';
+    deleteButton.addEventListener('click', (e) => {
+        deleteCard(e,cardDeck);
+    });
+    cardContainer.appendChild(deleteButton);
+    
+    // add up rank button
+    const upRankButton = cardButton('upRank');
+    upRankButton.textContent = 'Up Rank';
+    upRankButton.addEventListener('click', (e) => {
+        upRank(e,cardDeck);
+    });
+    cardContainer.appendChild(upRankButton);
+
+    // add down rank button
+    const downRankButton = cardButton('downRank');
+    downRankButton.textContent = 'Down Rank';
+    downRankButton.addEventListener('click', (e) => {
+        downRank(e,cardDeck);
+    });
+    cardContainer.appendChild(downRankButton);
+    
+    
+    return cardContainer;
+}
+
+function dashboardDisplay(cardDeck) {
+    
+    for (let cardIndex in cardDeck) {
+
+        let cardElement =  cardDisplay(cardDeck[cardIndex], cardDeck);
+        let dashboardID = `.dashboard-child#${cardDeck[cardIndex].status}`
+        
+        const dashboard = document.querySelector(dashboardID);
+        dashboard.appendChild(cardElement);
+
+    }
+
+}
+
+function clearDashboard() {
+    const dashboardStatus = ['planned', 'inProgress', 'completed'];
+    const titleMap = {planned : 'Planned', inProgress : 'In Progress', completed : 'Completed'};
+
+    for (let statusIndex in dashboardStatus) {
+        let dashboardID = `.dashboard-child#${dashboardStatus[statusIndex]}`
+        const dashboardElement = document.querySelector(dashboardID)
+        dashboardElement.replaceChildren()
+
+        // add back title element
+        const div = document.createElement('div');
+        div.setAttribute('class', `dashboard-title`);
+        div.textContent = titleMap[dashboardStatus[statusIndex]];
+        dashboardElement.appendChild(div);
+    }
+}
+
+function summaryValueDisplay(valueList) {
+    
+    const summaryValueSelection = document.getElementById('summary-value');
+
+    // clear previous display first
+    summaryValueSelection.replaceChildren()
+
+    // update value with current summary selection
+    for (let valueIndex in valueList){
+        let value = valueList[valueIndex];
+
+        const option = document.createElement('option');
+        option.setAttribute('value', value);
+        option.textContent = value;
+
+        summaryValueSelection.appendChild(option);
+    }
+
+}
+
+export { 
+    showInputDialog, closeInputDialog, 
+    dashboardDisplay, clearDashboard,
+    summaryValueDisplay
+}
